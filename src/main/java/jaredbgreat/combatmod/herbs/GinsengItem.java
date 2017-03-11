5d44312a414651b46eb92a1f33b556068a6fcdb1
Jared Blackburn (JaredBGreat) <BlackJar72@yahoo.com>
2017-03-11 01:25:41 -0500

    Ginseng is working

--- src/main/java/jaredbgreat/combatmod/herbs/GinsengItem.java
+++ src/main/java/jaredbgreat/combatmod/herbs/GinsengItem.java
@@ -2,9 +2,13 @@ package jaredbgreat.combatmod.herbs;
 
 import jaredbgreat.combatmod.Info;
 import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionHelper;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.common.EnumPlantType;
 import net.minecraftforge.common.IPlantable;
 
@@ -12,7 +16,8 @@ public class GinsengItem extends ItemFood implements IPlantable {
 
 	public GinsengItem(int hunger, float saturation) {
 		super(hunger, saturation, false);
-        setPotionEffect(PotionHelper.blazePowderEffect);
+        setPotionEffect("+0-1-2+3&4-4+13");
+        //setPotionEffect("+4"); // The netherwart value -- I *MAY* make a new, rarer herb for this!
         setUnlocalizedName(Info.ID + "-ginsingroot");
         this.setTextureName(Info.ID + ":Herb/ginseng");
 	}
@@ -30,6 +35,22 @@ public class GinsengItem extends ItemFood implements IPlantable {
 	@Override
 	public int getPlantMetadata(IBlockAccess world, int x, int y, int z) {
 		return 0;
+	}
+	
+	
+	@Override
+	public boolean onItemUse(ItemStack item, EntityPlayer player, World world, 
+							 int x, int y, int z, int what, float fx, float fy, float fz) {
+		Block target = world.getBlock(x, y, z);
+		if((target == Blocks.dirt) || (target == Blocks.grass) || (target == Blocks.farmland)) {
+			if(world.isAirBlock(x, y + 1, z)) {
+				world.setBlock(x, y + 1, z, Herbs.ginseng);
+				world.setBlockMetadataWithNotify(x, y + 1, z, 1, 2);
+				item.stackSize--;
+				return true;
+			}
+		}
+		return false;
 	}
 	
 }
--- src/main/java/jaredbgreat/combatmod/herbs/GinsengPlant.java
+++ src/main/java/jaredbgreat/combatmod/herbs/GinsengPlant.java
@@ -1,18 +1,27 @@
 package jaredbgreat.combatmod.herbs;
 
 import jaredbgreat.combatmod.Info;
+
+import java.util.Random;
+
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class GinsengPlant extends BlockHerb {
+	private IIcon[] icons;
 
 	protected GinsengPlant() {
 		setBlockName(Info.ID + "-GinsengPlant");
 		setBlockBounds(0.25f, 0f, 0.25f, 0.75f, 0.5f, 0.75f); // ???
         setBlockTextureName(Info.ID + ":Herb/Ginseng");
+        setLightOpacity(0);
 	}
     
 
@@ -30,17 +39,52 @@ public class GinsengPlant extends BlockHerb {
     public boolean onBlockActivated(World world, int x, int y, int z, 
     								EntityPlayer player, int side, 
     								float fx, float fy, float fz) {
-		world.setBlockToAir(x, y, z);
-    	if(world.isRemote) {
+    	if(player.getEquipmentInSlot(0) != null) {
+    		return false;
+    	}
+		if(world.isRemote) {
     		return true;
     	} else {
-    		ItemStack root = new ItemStack(Herbs.ginsengroot, world.rand.nextInt(3) + 1, 0);
+    		ItemStack root;
+    		if(world.getBlockMetadata(x, y, z) == 0) {
+    			root = new ItemStack(Herbs.ginsengroot, world.rand.nextInt(3) + 1, 0);
+    		} else {
+    			root = new ItemStack(Herbs.ginsengroot, 1, 0);
+    		}
         	EntityItem roots = new EntityItem(world, x + 0.5, y + 0.5, z + 0.5, root);
         	roots.delayBeforeCanPickup = 10;
         	world.spawnEntityInWorld(roots);
+        	world.setBlockToAir(x, y, z);        	
     		return true;
     	}
     }
     
     
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister ico) {
+    	icons = new IIcon[2];
+    	icons[0] = ico.registerIcon(Info.ID + ":Herb/Ginseng");
+    	icons[1] = ico.registerIcon(Info.ID + ":Herb/Ginseng_harvested");
+    }
+    
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int p1, int meta) {
+    	if(meta < 0 || meta >= 2) {
+    		return icons[0];
+    	} else {
+    		return icons[meta];
+    	}
+    }
+    
+    
+    @Override
+    public void updateTick(World world, int x, int y, int z, Random random) {
+    	if((world.getBlockLightValue(x, y, z) >=9) && (world.getBlockMetadata(x, y, z) != 0) 
+    			&& (random.nextInt(10) == 0)) {
+    		world.setBlockMetadataWithNotify(x, y, z, 0, 2);
+    	}
+    }
 }
